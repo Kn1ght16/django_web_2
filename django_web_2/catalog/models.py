20f68b9ea2e78bb0ba6ba3645b6aff5eb762f124
Kn1ght16 <Danchenko0@mail.ru>
2023-05-17 20:11:08 +0300

    0.6

--- django_web_2/catalog/models.py
+++ django_web_2/catalog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    image = models.ImageField(upload_to='product_images/', **NULLABLE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
