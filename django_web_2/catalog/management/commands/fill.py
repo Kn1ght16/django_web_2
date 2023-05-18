9cd41df408e4f2d62623f6e39b7cbf8e03f9d1f4
Kn1ght16 <Danchenko0@mail.ru>
2023-05-19 02:54:53 +0300

    0.8

--- django_web_2/catalog/management/commands/fill.py
+++ django_web_2/catalog/management/commands/fill.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand
-from django_web_2.django_web_2.catalog.models import Category, Product
+from catalog.models import Category, Product
 
 
 class Command(BaseCommand):
