aac410092166eab14e4732d7d7b7e2ce280b4084
Kn1ght16 <Danchenko0@mail.ru>
2023-06-09 05:27:34 +0300

    21.1 1

--- catalog/models.py
+++ catalog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 NULLABLE = {'blank': True, 'null': True}
 
@@ -9,8 +10,12 @@ class Category(models.Model):
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return f'{self.name}'
+
 
 class Product(models.Model):
+    DoesNotExist = None
     objects = None
     name = models.CharField(max_length=255)
     description = models.TextField()
@@ -22,3 +27,31 @@ class Product(models.Model):
 
     def __str__(self):
         return f"{self.name}"
+
+
+class Record(models.Model):
+    record_title = models.CharField(max_length=150, verbose_name='Заголовок')
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name="URL")
+    content = models.TextField(max_length=15000, verbose_name='Содержимое')
+    preview = models.ImageField(upload_to='image/', verbose_name='Изображение', **NULLABLE)
+    created_at = models.DateTimeField(auto_now=True, verbose_name='Дата создания')
+    sign_of_publication = models.BooleanField(default=True, verbose_name='Признак публикации')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(args, kwargs)
+        self.views = None
+
+    def __str__(self):
+        return f'{self.record_title}'
+
+    def get_absolute_url(self):
+        return reverse('record_detail', kwargs={'slug': self.slug})
+
+    class Meta:
+        verbose_name = 'запись'
+        verbose_name_plural = 'записи'
+        ordering = ('record_title', 'slug', 'created_at', 'sign_of_publication')
+
+    def increase_views(self):
+        self.views += 1
+        self.save()
--- catalog/urls.py
+++ catalog/urls.py
@@ -3,7 +3,14 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('contacts/', views.contacts, name='contacts'),
-    path('product/<int:product_id>/', views.Product, name='product'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('contacts/', views.ContactView.as_view(), name='contacts'),
+    path('product/<int:pk>/', views.ProductDetailView.as_view(), name='product'),
+    path('products/', views.ProductListView.as_view(), name='products'),
+    path('records/', views.RecordListView.as_view(), name='records_list'),
+    path('records/<slug:slug>/', views.RecordDetailView.as_view(), name='record_detail'),
+    path('record/create/', views.RecordCreateView.as_view(), name='record_create'),
+    path('record/update/<int:pk>/', views.RecordUpdateView.as_view(), name='record_update'),
+    path('record/delete/<int:pk>/', views.RecordDeleteView.as_view(), name='record_delete'),
+    path('product/item/<int:pk>/', views.ProductItemView.as_view(), name='product_item'),
 ]
